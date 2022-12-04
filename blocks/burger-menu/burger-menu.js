d9454cdac7c46efe0c95698e053b9e13c0ef114c
Aleksandr Zaytsev <azaycev4@mail.ru>
2022-12-04 12:17:36 +0600

    fix(styles): update bem structure

---
+++ blocks/burger-menu/burger-menu.js
@@ -0,0 +1,19 @@
+const openBurgerButton = document.querySelector(".burger-button--open");
+const closedBurgerButton = document.querySelector(".burger-button--closed");
+
+const burgerMenuContainer = document.querySelector(".burger-menu__container");
+
+const checkIsOpenMenu = () => {
+  if (burgerMenuContainer.classList.contains("burger-menu--closed")) {
+    burgerMenuContainer.classList.remove("burger-menu--closed");
+    burgerMenuContainer.classList.add("burger-menu--open");
+  } else {
+    burgerMenuContainer.classList.remove("burger-menu--open");
+    burgerMenuContainer.classList.add("burger-menu--closed");
+  }
+};
+
+burgerMenuContainer.classList.add("burger-menu--closed");
+
+openBurgerButton.addEventListener("click", checkIsOpenMenu);
+closedBurgerButton.addEventListener("click", checkIsOpenMenu);
