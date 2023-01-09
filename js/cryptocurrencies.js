30a43cc5c2793ac22b3bf3a71ea2c47ce34ed20c
Aleksandr Zaytsev <azaycev4@mail.ru>
2023-01-09 09:11:02 +0600

    fix(index): refactoring

--- js/cryptocurrencies.js
+++ js/cryptocurrencies.js
@@ -5,12 +5,8 @@ const seeAllButton = document.querySelector(
 );
 seeAllButton.classList.add("primary-button--hide");
 
-const stateSeeAllOpen = document.querySelector(
-  ".cryptocurrencies__see-all-arrow-icon--open"
-);
-const stateSeeAllClosed = document.querySelector(
-  ".cryptocurrencies__see-all-arrow-icon--closed"
-);
+const stateSeeAllOpen = document.querySelector(".see-all-button--open");
+const stateSeeAllClosed = document.querySelector(".see-all-button--closed");
 
 const currenciesListArray = document.querySelectorAll(
   ".cryptocurrencies__list"
@@ -21,33 +17,26 @@ if (window.innerWidth < 1024) {
   currenciesListArray[3].classList.add("cryptocurrencies--hide-list");
 
   seeAllButton.classList.remove("primary-button--hide");
-  stateSeeAllOpen.classList.add("cryptocurrencies__see-all-arrow-icon--hide");
 
   seeAllButton.addEventListener("click", () => {
     if (!isOpenSeeAll) {
       currenciesListArray[2].classList.remove("cryptocurrencies--hide-list");
       currenciesListArray[3].classList.remove("cryptocurrencies--hide-list");
 
-      stateSeeAllOpen.classList.remove(
-        "cryptocurrencies__see-all-arrow-icon--hide"
-      );
-      stateSeeAllClosed.classList.add(
-        "cryptocurrencies__see-all-arrow-icon--hide"
-      );
+      seeAllButton.classList.add("see-all-button--closed");
+      seeAllButton.classList.remove("see-all-button--open");
 
       isOpenSeeAll = true;
     } else {
       currenciesListArray[2].classList.add("cryptocurrencies--hide-list");
       currenciesListArray[3].classList.add("cryptocurrencies--hide-list");
 
-      stateSeeAllOpen.classList.add(
-        "cryptocurrencies__see-all-arrow-icon--hide"
-      );
-      stateSeeAllClosed.classList.remove(
-        "cryptocurrencies__see-all-arrow-icon--hide"
-      );
+      seeAllButton.classList.add("see-all-button--open");
+      seeAllButton.classList.remove("see-all-button--closed");
 
       isOpenSeeAll = false;
     }
   });
+} else {
+  seeAllButton.classList.add("cryptocurrencies__see-all-arrow-icon--hide");
 }
